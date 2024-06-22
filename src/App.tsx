7363bc97d3e06377e7006e9c24c56f20f52f0cfc
Sesslyn041215 <sesslyn2004@gmail.com>
2024-06-22 13:43:08 +0530

    fix(chatbot): Chatbot center

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from "react";
-import Chatbot from "./Chatbot";
+import React, { useState } from 'react';
+import Chatbot from './Chatbot';
 import './index.css';
 
 const App: React.FC = () => {
+  const [showChart, setShowChart] = useState(false);
+
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '90vh',
+    width: '85vw',
+    margin: 'auto', 
+  };
+
+  const chatbotContainerStyle = {
+    marginLeft: showChart ? 'auto' : '0px',
+  };
+
   return (
-    <div className="component">
-      <div className="box">
-        <h1>Chatbot-Chart</h1>
-      </div>
-      <div className="chatbot-container">
-        <Chatbot />
+    <div className="component" style={showChart ? {} : containerStyle}>
+      {showChart && (
+        <div className="box">
+          <h1>Chatbot-Chart</h1>
+        </div>
+      )}
+      <div className="chatbot-container" style={chatbotContainerStyle}>
+        <Chatbot showChart={showChart} setShowChart={setShowChart} />
       </div>
     </div>
   );
